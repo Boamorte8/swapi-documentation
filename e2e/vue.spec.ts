d87522983474541840a5a1b4dfbddcf9158721c9
Esteban Salazar Boamorte <salazar.z.esteban@gmail.com>
2023-05-23 16:57:03 -0500

    test(project): :white_check_mark: add new e2e tests

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -1,8 +1,69 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 // See here how to get started:
 // https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
+test('visits the app home url', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
+
+  await expect(page.locator('header > img')).toHaveAttribute('alt', 'Star Wars logo');
+  await expect(page.locator('header > img')).toHaveAttribute('src', /swlogo.webp/);
+  await expect(page.locator('header > h1')).toHaveText('Star Wars Documentation');
+  await expect(page.locator('header > p')).toHaveText('In this page you will find information about Star Wars');
+
+  await expect(page.locator('main > article')).toHaveCount(3);
+
+  await expect(page.locator('footer')).toHaveText('Made by Esteban Salazar Zapata');
+})
+
+test('visits the films page', async ({ page }) => {
+  await page.goto('/');
+
+  await page.locator('article').first().click();
+
+  await expect(page).toHaveURL(/.*films/);
+
+  await expect(page.locator('h1')).toHaveText('Films');
+  await expect(page.locator('p')).toHaveText('These are all films that you can find on Start Wars');
+
+  await page.getByRole('link', { name: 'Back' }).click();
+
+  await expect(page.locator('header > img')).toHaveAttribute('alt', 'Star Wars logo');
+  await expect(page.locator('header > img')).toHaveAttribute('src', /swlogo.webp/);
+  await expect(page.locator('header > h1')).toHaveText('Star Wars Documentation');
+  await expect(page.locator('header > p')).toHaveText('In this page you will find information about Star Wars');
+})
+
+test('visits the planets page', async ({ page }) => {
+  await page.goto('/');
+
+  await page.locator('article').filter({ hasText: 'Planets' }).click();
+
+  await expect(page).toHaveURL(/.*planets/);
+
+  await expect(page.locator('h1')).toHaveText('Planets');
+  await expect(page.locator('p')).toHaveText('These are planets that you can find on Start Wars');
+
+  await page.getByRole('link', { name: 'Back' }).click();
+
+  await expect(page.locator('header > img')).toHaveAttribute('alt', 'Star Wars logo');
+  await expect(page.locator('header > img')).toHaveAttribute('src', /swlogo.webp/);
+  await expect(page.locator('header > h1')).toHaveText('Star Wars Documentation');
+  await expect(page.locator('header > p')).toHaveText('In this page you will find information about Star Wars');
+})
+
+test('visits the questions page', async ({ page }) => {
+  await page.goto('/');
+
+  await page.locator('article').last().click();
+
+  await expect(page).toHaveURL(/.*questions/);
+
+  await expect(page.locator('h1')).toHaveText('Questions');
+
+  await page.getByRole('link', { name: 'Back' }).click();
+
+  await expect(page.locator('header > img')).toHaveAttribute('alt', 'Star Wars logo');
+  await expect(page.locator('header > img')).toHaveAttribute('src', /swlogo.webp/);
+  await expect(page.locator('header > h1')).toHaveText('Star Wars Documentation');
+  await expect(page.locator('header > p')).toHaveText('In this page you will find information about Star Wars');
 })
